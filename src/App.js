d8ea7efcf89d29560733b6ba3c3211f5c1bb2ab1
ivanbaug <ivsitioinfo@gmail.com>
2022-03-08 16:58:38 -0500

    add user display components and first fetch

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Footer from './components/layout/Footer'
 import Navbar from './components/layout/Navbar'
+import About from './pages/About'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
 
 function App() {
   return (
@@ -10,7 +13,10 @@ function App() {
         <Navbar />
         <main className="container mx-auto px-3 pb-12">
           <Routes>
-            <Route path="/" exact element={<>Hello world!</>} />
+            <Route path="/" exact element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
           </Routes>
         </main>
         <Footer />
