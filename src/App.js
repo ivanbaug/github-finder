ece6d48252f0109c446f6886d7bf9b9af62970a5
ivanbaug <ivsitioinfo@gmail.com>
2022-03-07 22:13:54 -0500

    add tailwind and create navbar and footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Footer from './components/layout/Footer'
+import Navbar from './components/layout/Navbar'
+
 function App() {
   return (
-    <div className="bg-purple-500">
-      <h1 className="text-3xl font-bold underline bg-purple-500">
-        Hello world!
-      </h1>
-      <button className="btn">hello again</button>
-    </div>
+    <BrowserRouter>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" exact element={<>Hello world!</>} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
   )
 }
 
