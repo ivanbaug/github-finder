70825b66e5c9f061eaa4b9f2191cd946dc586b69
ivanbaug <ivsitioinfo@gmail.com>
2022-03-12 23:51:39 -0500

    add repos display and create actions for context

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,48 +1,23 @@
 import { createContext, useReducer } from 'react'
 import githubReducer from './GithubReducer'
 
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
-
 const GithubContext = createContext()
 
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
     loading: false,
+    repos: [],
+    user: {},
   }
 
   const [state, dispatch] = useReducer(githubReducer, initialState)
 
-  //  Get search results
-  const searchUsers = async (text) => {
-    setLoading()
-
-    const params = new URLSearchParams({
-      q: text,
-    })
-    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    })
-    const { items } = await response.json()
-    dispatch({ type: 'GET_USERS', payload: items })
-  }
-
-  // Set loading
-  const setLoading = () => dispatch({ type: 'SET_LOADING' })
-
-  // Clear Users
-  const clearUsers = () => dispatch({ type: 'CLEAR_USERS' })
-
   return (
     <GithubContext.Provider
       value={{
-        users: state.users,
-        loading: state.loading,
-        searchUsers,
-        clearUsers,
+        ...state,
+        dispatch,
       }}
     >
       {children}
